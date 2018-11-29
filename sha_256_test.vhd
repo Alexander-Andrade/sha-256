3ca7112c2a73290ebc8e88e64895edecee826f6b
Alexander-Andrade <veimar.94@gmail.com>
2018-11-29 23:16:37 +0300

    init commit

--- sha_256_test.vhd
+++ sha_256_test.vhd
@@ -10,7 +10,11 @@ architecture Behavioral of sha_256_test is
         Port ( message : in  STD_LOGIC_VECTOR (255 downto 0);
                digest : out  STD_LOGIC_VECTOR (255 downto 0));
     end component;
+    
+    signal test_message : STD_LOGIC_VECTOR (255 downto 0) := (1 downto 0 => '1', others => '0');
+    signal hash_output : STD_LOGIC_VECTOR (255 downto 0) := (others => '0');
 begin
 
+    sha: sha_256 port map ( message => test_message, digest => hash_output);
 
 end Behavioral;
